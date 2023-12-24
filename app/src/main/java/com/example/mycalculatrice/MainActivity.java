96248eae8826023d7e2b5362353c3251d9e443eb
amanicheriett <115952757+amanicheriett@users.noreply.github.com>
2023-12-24 10:55:12 +0100

    first commit

---
+++ app/src/main/java/com/example/mycalculatrice/MainActivity.java
@@ -0,0 +1,131 @@
+package com.example.mycalculatrice;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText opr1;
+    private EditText opr2;
+    private Button btnadd;
+    private  Button btnsub;
+    private  Button btnmul;
+    private Button btndiv;
+    private EditText txtresult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        opr1 = (EditText) findViewById(R.id.editOp1);
+        opr2 = (EditText) findViewById(R.id.editOp2);
+        btnadd = (Button) findViewById(R.id.btnadd);
+        btnsub = (Button) findViewById(R.id.btnsub);
+        btnmul = (Button) findViewById(R.id.btnmul);
+        btndiv = (Button) findViewById(R.id.btndiv);
+        txtresult= (EditText) findViewById(R.id.result);
+
+        // Addition
+        btnadd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if((opr1.getText().length()>0) && (opr2.getText().length()>0))
+                {
+                    Double oper1 = Double.parseDouble(opr1.getText().toString());
+                     Double oper2 = Double.parseDouble(opr2.getText().toString());
+                   Double result = oper1 + oper2;
+                    txtresult.setText(Double.toString(result));
+                }
+                else{
+                    Toast toast= Toast.makeText(MainActivity.this,"Enter The Required Numbers",Toast.LENGTH_LONG);
+                    toast.show();
+                }
+            }
+        });
+        //Subtraction
+        btnsub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if((opr1.getText().length()>0) && (opr2.getText().length()>0))
+                {
+                    double oper1 = Double.parseDouble(opr1.getText().toString());
+                    double oper2 = Double.parseDouble(opr2.getText().toString());
+                    double result = oper1 - oper2;
+                    txtresult.setText(Double.toString(result));
+                }
+                else{
+                    Toast toast= Toast.makeText(MainActivity.this,"Enter The Required Numbers",Toast.LENGTH_LONG);
+                    toast.show();
+                }
+
+            }
+        });
+        // Multiplication
+        btnmul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if((opr1.getText().length()>0) && (opr2.getText().length()>0))
+                {
+                    double oper1 = Double.parseDouble(opr1.getText().toString());
+                    double oper2 = Double.parseDouble(opr2.getText().toString());
+                    double result = oper1 * oper2;
+                    txtresult.setText(Double.toString(result));
+                }
+                else{
+                    Toast toast= Toast.makeText(MainActivity.this,"Enter The Required Numbers",Toast.LENGTH_LONG);
+                    toast.show();
+                }
+            }
+        });
+        // Division
+        btndiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if((opr1.getText().length()>0) && (opr2.getText().length()>0))
+                {
+                    double oper1 = Double.parseDouble(opr1.getText().toString());
+                    double oper2 = Double.parseDouble(opr2.getText().toString());
+                    double result = oper1 / oper2;
+                    txtresult.setText(Double.toString(result));
+                }
+                else{
+                    Toast toast= Toast.makeText(MainActivity.this,"Enter The Required Numbers",Toast.LENGTH_LONG);
+                    toast.show();
+                }
+            }
+        });
+
+
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem Item){
+
+        switch (Item.getItemId()) {
+            case R.id.revenir:
+                opr1.setText(null);
+                opr2.setText(null);
+                txtresult.setText(null);
+                Toast.makeText(this, "champs retablis au null, remplir les champs svp ", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.Quitter:
+                finish();
+                Toast.makeText(this, "vous n'etes plus sur my calculatrice", Toast.LENGTH_SHORT).show();
+            default:
+                return super.onOptionsItemSelected(Item);
+        }
+
+        }
+
+    }
